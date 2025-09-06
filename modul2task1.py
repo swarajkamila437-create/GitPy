c1e1e5dc91f4a4dd41fa112348a8b54f1b357e26
swarajkamila437-create <swarajkamila437@gmail.com>
2025-09-06 21:30:42 +0530

    Add files via upload

---
+++ modul2task1.py
@@ -0,0 +1,7 @@
+#Mathematical Operation
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("Addition: ",num1 + num2)
+print("Subtraction: ",num1 - num2)
+print("Multiplication: ",num1 * num2)
+print("Division: ",num1/num2)
---
+++ modul2task2.py
@@ -0,0 +1,5 @@
+#Full name with a greeting message
+a=input("Enter First name: ")
+b=input("Enter Last name: ")
+name = a + b 
+print("Hello",name,"Welcome to the Python program")
